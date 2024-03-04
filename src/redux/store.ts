9725314afba809c3d54d016c6fa5254362559a22
DimaDzh <dzhartyds@gmail.com>
2024-03-04 19:00:19 +0200

    updated save to session storage

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,16 +3,29 @@ import { createStore, Store } from "redux";
 import todoReducer, { TodoState } from "./reducer";
 import { TodoAction } from "./actionTypes";
 
-const initialState: TodoState = {
-  todos: [],
-  completedCount: 0,
-  incompleteCount: 0,
-  filter: "ALL",
-};
+// Get the state from session storage, if it exists
+const savedState = sessionStorage.getItem("reduxState");
+const initialState: TodoState = savedState
+  ? JSON.parse(savedState)
+  : {
+      todos: [],
+      completedCount: 0,
+      incompleteCount: 0,
+      filter: "ALL",
+    };
 
 const store: Store<TodoState, TodoAction> = createStore(
   todoReducer,
   initialState
 );
+
+// Save store to session storage whenever it changes
+store.subscribe(() => {
+  // Get the current state from the store
+  const currentState = store.getState();
+
+  // Save the state to session storage
+  sessionStorage.setItem("reduxState", JSON.stringify(currentState));
+});
 
 export default store;
